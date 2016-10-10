27fd7af11451985e3b558f4eb4d12b3c396a578d
jasmi <jasmi@DESKTOP-T3PI036>
2016-10-10 15:54:29 -0400

    Week 3

---
+++ Ext/Ext/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ext
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string whatEver = "I am looking for all the letter a's";
+            int howMany = whatEver.Looking();
+            Console.WriteLine(howMany);
+
+            int huh = 7;
+            int anotherHuh = huh.Plus();
+            Console.WriteLine(anotherHuh);
+
+            double day = 12;
+            double anotherDay = day.Money();
+            Console.WriteLine(anotherDay);
+
+            float sum = 7;
+            float anotherSum = sum.Div();
+            Console.WriteLine(anotherSum);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Ext/Ext/Whatsup.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ext
+{
+    public static class Whatsup
+    {
+
+        public static int Looking(this string find)
+        {
+            int where = 0;
+            foreach (char c in find)
+            {
+                if (c == 'a')
+                    where++;
+            }
+            return where;
+        }
+
+        public static int Plus(this int add)
+        {
+            int grow = add + 2;
+            return grow;
+        }
+
+        public static double Money(this double pay)
+        {
+            double grow = pay + .50;
+            return grow;
+        }
+
+        public static bool WhatsUpYo(this bool thesky)
+        {
+            if (thesky == false)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static bool Maybe(this char myb)
+        {
+            if (myb == 'n')
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static float Div(this float product)
+        {
+            float flt = product / 3;
+            return flt;
+        }
+    }
+}
